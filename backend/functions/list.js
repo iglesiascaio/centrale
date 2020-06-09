931ce56b8fc044a28d956804b2f8abeb3cd7526d
Kledeglau Jahchan Alves Matheus <matheus.kja@student-cs.fr>
2020-06-09 22:28:20 +0200

    lIST

--- backend/functions/list.js
+++ backend/functions/list.js
@@ -1,21 +1,7 @@
-const DynamoDB = require('aws-sdk/clients/dynamodb');
 
 module.exports.handle = async event => {
-    if (!process.env.tableName) {
-        throw new Error('env.tableName must be defined');
-    }
 
-    const dynamoDb = new DynamoDB.DocumentClient();
-    const result = await dynamoDb.query({
-        TableName: process.env.tableName,
-        KeyConditionExpression: '#type = :type',
-        ExpressionAttributeNames: {
-            '#type': 'type'
-        },
-        ExpressionAttributeValues: {
-            ':type': 'items',
-        },
-    }).promise();
+    let vamo = ['vamoo','pff']
 
     return {
         statusCode: 200,
@@ -23,6 +9,6 @@ module.exports.handle = async event => {
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Credentials': true,
         },
-        body: JSON.stringify(result.Items),
+        body: JSON.stringify(vamo),
     }
 }
