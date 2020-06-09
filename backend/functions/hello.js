d8e4bb70251b5927de8c6b95a1aefdc65e3e1cf0
Kledeglau Jahchan Alves Matheus <matheus.kja@student-cs.fr>
2020-06-09 11:21:26 +0200

    Fixing headers

--- backend/functions/hello.js
+++ backend/functions/hello.js
@@ -1,11 +1,16 @@
 'use strict';
 
 module.exports.handle = async event => {
+
   return {
     statusCode: 200,
+    headers:{
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: 'Caio OTARIO',
         input: event,
       },
       null,
